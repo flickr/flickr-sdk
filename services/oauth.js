893ea598809bcb692a76e412a96c75b0e4cbbc9c
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2016-10-26 09:38:59 -0700

    Create an OAuth service class

---
+++ services/oauth.js
@@ -0,0 +1,160 @@
+var request = require('superagent');
+var crypto = require('crypto');
+
+/**
+ * Returns a nonce for the timestamp `time`. OAuth 1.0 defines a
+ * nonce as a value unique to a given timestamp.
+ * @param {Number} time
+ * @returns {String}
+ * @see https://oauth.net/core/1.0a/#nonce
+ * @private
+ */
+
+function nonce(time) {
+	return crypto.createHash('sha1').update(String(time)).digest('hex');
+}
+
+/**
+ * Returns the HMAC-SHA1 digest of `text` and `key` in baset64 encoding.
+ * @param {String} text
+ * @param {String} key
+ * @returns {String}
+ * @see https://oauth.net/core/1.0a/#rfc.section.9.2
+ * @private
+ */
+
+function hmac(text, key) {
+	return crypto.createHmac('sha1', key).update(text).digest('base64');
+}
+
+/**
+ * Encodes each of the strings in `arr` and joins them with the '&'
+ * character (ASCII code 38).
+ * @param {String[]} arr
+ * @returns {String}
+ * @see https://oauth.net/core/1.0a/#rfc.section.9.1.3
+ * @see https://oauth.net/core/1.0a/#encoding_parameters
+ * @private
+ */
+
+function join(arr) {
+	return arr.map(encodeURIComponent).join('&');
+}
+
+/**
+ * Normalizes and sorts all of the query string params in `req`,
+ * then joins them with the '&' character (ASCII code 38).
+ * @param {Request}
+ * @returns String.
+ * @see https://oauth.net/core/1.0a/#sig_norm_param
+ * @private
+ */
+
+function query(req) {
+	var pairs = req.qsRaw.slice();
+
+	Object.keys(req.qs).forEach(function (key) {
+		pairs.push(key + '=' + encodeURIComponent(req.qs[key]));
+	});
+
+	return pairs.sort().join('&');
+}
+
+/**
+ * @constructor
+ * @param {String} consumerKey The application's API key
+ * @param {String} consumerSecret The application's API secret
+ */
+
+function OAuth(consumerKey, consumerSecret) {
+	this.consumerKey = consumerKey;
+	this.consumerSecret = consumerSecret;
+}
+
+/**
+ * @module services/oauth
+ */
+
+module.exports = OAuth;
+
+/**
+ * Get a Request Token using the consumer key.
+ * @param {String} oauthCallback
+ * @returns {Request}
+ * @see https://github.com/visionmedia/superagent
+ * @see https://www.flickr.com/services/api/auth.oauth.html#request_token
+ */
+
+OAuth.prototype.request = function (oauthCallback) {
+	return request('GET', 'https://www.flickr.com/services/oauth/request_token')
+	.query({ oauth_callback: oauthCallback })
+	.query(this.params())
+	.parse(this.parse)
+	.use(this.sign());
+
+	/*
+		TODO 'https://www.flickr.com/services/oauth/authorize?oauth_token=' + res.body.oauth_token
+	*/
+
+};
+
+/**
+ * Verify an OAuth token using the verifier and token secret.
+ * @param {String} oauthToken
+ * @param {String} oauthVerifier
+ * @param {String} tokenSecret
+ * @returns {Request}
+ * @see https://github.com/visionmedia/superagent
+ * @see https://www.flickr.com/services/api/auth.oauth.html#access_token
+ */
+
+OAuth.prototype.verify = function (oauthToken, oauthVerifier, tokenSecret) {
+	return request('GET', 'https://www.flickr.com/services/oauth/access_token')
+	.query({ oauth_token: oauthToken, oauth_verifier: oauthVerifier })
+	.query(this.params())
+	.parse(this.parse)
+	.use(this.sign(tokenSecret));
+};
+
+/**
+ * For some OAuth endpoints, the API returns the content-type
+ * as "text/plain;charset=UTF-8" when really it should be
+ * "application/x-www-form-urlencoded". This getter simply
+ * returns the superagent standard form/urlencoded parser.
+ * @type {Function}
+ */
+
+OAuth.prototype.parse = request.parse['application/x-www-form-urlencoded'];
+
+/**
+ * Creates an object with the standard OAuth 1.0 query params
+ * for this instance.
+ * @returns {Object}
+ */
+
+OAuth.prototype.params = function () {
+	return {
+		oauth_nonce: nonce(Date.now()),
+		oauth_timestamp: Date.now(), // FIXME shouldn't this be seconds instead of milliseconds?
+		oauth_consumer_key: this.consumerKey,
+		oauth_signature_method: 'HMAC-SHA1',
+		oauth_version: '1.0'
+	};
+};
+
+/**
+ * TODO docs
+ */
+
+OAuth.prototype.sign = function (tokenSecret) {
+	var consumerSecret = this.consumerSecret;
+
+	return function (req) {
+		var signingKey = join([ consumerSecret, tokenSecret || '' ]);
+		var baseString = join([ req.method, req.url, query(req) ]);
+
+		req.query({
+			oauth_signature: hmac(baseString, signingKey)
+		});
+	};
+};
---
+++ test/services.oauth.js
@@ -0,0 +1,126 @@
+var OAuth = require('../services/oauth');
+var request = require('superagent');
+var assert = require('assert');
+var time = require('timemachine');
+var nock = require('nock');
+
+describe('services/oauth', function () {
+	var subject;
+
+	beforeEach(function () {
+		subject = new OAuth('consumer key', 'consumer secret');
+	});
+
+	beforeEach(function () {
+		time.config({
+			dateString: 'October 26, 1985 01:20:00'
+		});
+	});
+
+	afterEach(function () {
+		time.reset();
+	});
+
+	describe('#request', function () {
+
+		it('makes the correct API call', function () {
+			var api = nock('https://www.flickr.com')
+			.get('/services/oauth/request_token')
+			.query({
+				oauth_nonce: '0d6e24f8073fe797e4d0187b95a141543e43698d',
+				oauth_timestamp: 499162800000,
+				oauth_consumer_key: subject.consumerKey,
+				oauth_signature_method: 'HMAC-SHA1',
+				oauth_version: '1.0',
+				oauth_callback: 'https://www.example.com/callback',
+				oauth_signature: 'qQ3gdACX0PrmzVAYIL6NjjrkYMw='
+			})
+			.reply(200, 'oauth_callback_confirmed=true&oauth_token=foo&oauth_token_secret=bar');
+
+			return subject.request('https://www.example.com/callback').then(function (res) {
+				assert(api.isDone());
+				assert.equal(res.statusCode, 200);
+				assert.equal(res.body.oauth_callback_confirmed, 'true');
+				assert.equal(res.body.oauth_token, 'foo');
+				assert.equal(res.body.oauth_token_secret, 'bar');
+			});
+
+		});
+
+	});
+
+	describe('#verify', function () {
+
+		it('makes the correct API call', function () {
+			var api = nock('https://www.flickr.com')
+			.get('/services/oauth/access_token')
+			.query({
+				oauth_token: 'token',
+				oauth_verifier: 'verfier',
+				oauth_nonce: '0d6e24f8073fe797e4d0187b95a141543e43698d',
+				oauth_timestamp: 499162800000,
+				oauth_consumer_key: subject.consumerKey,
+				oauth_signature_method: 'HMAC-SHA1',
+				oauth_version: '1.0',
+				oauth_signature: 'Qyhwc9332I2rv4OT/ecfI6yNvAM='
+			})
+			.reply(200, 'fullname=Jamal%20Fanaian&oauth_token=foo&oauth_token_secret=bar&user_nsid=21207597%40N07&username=jamalfanaian');
+
+			return subject.verify('token', 'verfier', 'tokenSecret').then(function (res) {
+				assert(api.isDone());
+				assert.equal(res.statusCode, 200);
+				assert.equal(res.body.fullname, 'Jamal Fanaian');
+				assert.equal(res.body.oauth_token, 'foo');
+				assert.equal(res.body.oauth_token_secret, 'bar');
+				assert.equal(res.body.user_nsid, '21207597@N07');
+				assert.equal(res.body.username, 'jamalfanaian');
+			});
+		});
+
+	});
+
+	describe('#params', function () {
+
+		it('returns OAuth 1.0 params', function () {
+			var params = subject.params();
+
+			assert.equal(params.oauth_nonce, '0d6e24f8073fe797e4d0187b95a141543e43698d');
+			assert.equal(params.oauth_timestamp, 499162800000);
+			assert.equal(params.oauth_consumer_key, subject.consumerKey);
+			assert.equal(params.oauth_signature_method, 'HMAC-SHA1');
+			assert.equal(params.oauth_version, '1.0');
+		});
+
+	});
+
+	describe('#sign', function () {
+
+		it('returns a superagent plugin', function () {
+			assert.equal(typeof subject.sign(), 'function');
+		});
+
+		it('signs a request without a token secret', function () {
+			var req = request('GET', 'http://www.example.com');
+
+			req.query({ foo: '123' });
+			req.query('bar=456');
+
+			subject.sign()(req);
+
+			assert.equal(req.qs.oauth_signature, 'lNBTzyWRRHBuoXmgK13Nht5oiKs=');
+		});
+
+		it('signs a request with a token secret', function () {
+			var req = request('GET', 'http://www.example.com');
+
+			req.query({ foo: '123' });
+			req.query('bar=456');
+
+			subject.sign('keyboard cat')(req);
+
+			assert.equal(req.qs.oauth_signature, 'pytjoWTzMmvq13/Bai9YVX1tV9c=');
+		});
+
+	});
+
+});
