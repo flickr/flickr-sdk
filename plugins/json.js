a9590ddd87d975056f8e14723f993dc902843f5d
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2016-11-04 15:20:10 -0700

    Refactor Flickr JSON parser into its own module

---
+++ plugins/json.js
@@ -0,0 +1,35 @@
+var parseJSON = require('superagent/lib/node/parsers/json');
+
+/**
+ * Custom response parser routine to handle Flickr API-style
+ * error responses. The Flickr API has a whole bunch of client
+ * error codes, but they all come back as HTTP 200 responses.
+ * Here, we extend the normal JSON response parser and check
+ * for a Flickr API error. If we find one, craft a new error
+ * out of that and yield it.
+ * @param {Response} res
+ * @param {Function} fn
+ * @returns {null}
+ */
+
+function parseFlickr(res, fn) {
+	parseJSON(res, function (err, body) {
+		if (err) {
+			return fn(err, body);
+		}
+
+		if (body.stat === 'fail') {
+			err = new Error(body.message);
+			err.stat = body.stat;
+			err.code = body.code;
+		}
+
+		fn(err, body);
+	});
+}
+
+module.exports = function (req) {
+	req.query('format=json');
+	req.query('nojsoncallback=1');
+	req.parse(parseFlickr);
+};
--- request.js
+++ request.js
@@ -1,33 +1,5 @@
 var request = require('superagent');
-var parseJSON = require('superagent/lib/node/parsers/json');
-
-/**
- * Custom response parser routine to handle Flickr API-style
- * error responses. The Flickr API has a whole bunch of client
- * error codes, but they all come back as HTTP 200 responses.
- * Here, we extend the normal JSON response parser and check
- * for a Flickr API error. If we find one, craft a new error
- * out of that and yield it.
- * @param {Response} res
- * @param {Function} fn
- * @returns {null}
- */
-
-function parseFlickr(res, fn) {
-	parseJSON(res, function (err, body) {
-		if (err) {
-			return fn(err, body);
-		}
-
-		if (body.stat === 'fail') {
-			err = new Error(body.message);
-			err.stat = body.stat;
-			err.code = body.code;
-		}
-
-		fn(err, body);
-	});
-}
+var json = require('./plugins/json');
 
 /**
  * Creates a new Flickr API client. This "client" is a factory
@@ -35,7 +7,7 @@ function parseFlickr(res, fn) {
  * for talking to the Flickr API. You must pass your `api_key`
  * either as a string or as a field on a hash of default query
  * string params.
- * @param {String|Object} defaults
+ * @param {(String|Object)} defaults
  * @returns {Function}
  * @see https://github.com/visionmedia/superagent
  */
@@ -54,12 +26,10 @@ module.exports = function createClient(defaults) {
 		}
 
 		return request(verb, 'https://api.flickr.com/services/rest')
-		.query(defaults)
 		.query('method=' + method)
-		.query('format=json')
-		.query('nojsoncallback=1')
-		.query(args)
-		.parse(parseFlickr);
+		.use(json)
+		.query(defaults)
+		.query(args);
 	};
 
 };
