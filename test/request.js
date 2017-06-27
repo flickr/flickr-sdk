3cde14fdeea6824f3b474f173f112e934338b7cb
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2017-06-27 13:12:29 -0700

    100% coverage for request factory

--- test/request.js
+++ test/request.js
@@ -1,16 +1,29 @@
-var subject = require('../request')(function auth() { /* noop */ });
+var subject = require('../request');
 var Request = require('superagent').Request;
 var assert = require('assert');
 var parse = require('url').parse;
 
+describe('request factory', function () {
+
+	it('requires an auth function to be passed', function () {
+		assert.throws(function () {
+			subject();
+		}, function (err) {
+			return err.message === 'Missing auth superagent plugin';
+		});
+	});
+
+});
+
 describe('request', function () {
+	var request = subject(function auth() { /* noop for tests */ });
 
 	it('returns a superagent Request', function () {
-		assert(subject('GET', 'flickr.test.echo') instanceof Request);
+		assert(request('GET', 'flickr.test.echo') instanceof Request);
 	});
 
 	it('adds default request headers', function () {
-		var req = subject('GET', 'flickr.test.echo').request();
+		var req = request('GET', 'flickr.test.echo').request();
 
 		/*
 			TODO user-agent
@@ -19,12 +32,24 @@ describe('request', function () {
 		assert.equal(req.getHeader('x-flickr-api-method'), 'flickr.test.echo');
 	});
 
-	it('uses the correct path');
-	it('uses the correct host');
+	it('uses the correct path', function () {
+		var req = request('GET', 'flickr.test.echo');
+		var url = parse(req.url);
+
+		assert.equal(url.pathname, '/services/rest');
+	});
+
+	it('uses the correct host', function () {
+		var req = request('GET', 'flickr.test.echo');
+		var url = parse(req.url);
+
+		assert.equal(url.host, 'api.flickr.com');
+	});
+
 	it('can provide the host as an option');
 
 	it('adds default query string arguments', function () {
-		var req = subject('GET', 'flickr.test.echo').request();
+		var req = request('GET', 'flickr.test.echo').request();
 		var url = parse(req.path, true);
 
 		assert.equal(url.query.method, 'flickr.test.echo');
@@ -33,7 +58,7 @@ describe('request', function () {
 	});
 
 	it('adds additional query string arguments', function () {
-		var req = subject('GET', 'flickr.test.echo', { foo: 'bar' }).request();
+		var req = request('GET', 'flickr.test.echo', { foo: 'bar' }).request();
 		var url = parse(req.path, true);
 
 		assert.equal(url.query.method, 'flickr.test.echo');
@@ -43,7 +68,7 @@ describe('request', function () {
 	});
 
 	it('joins "extras" if passed as an array', function () {
-		var req = subject('GET', 'flickr.test.echo', {
+		var req = request('GET', 'flickr.test.echo', {
 			extras: [
 				'foo',
 				'bar',
