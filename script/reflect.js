7eeda1afa3132809026fbd8c94f1a8baa20d9779
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2017-07-31 11:12:33 -0700

    Update reflect script to use built sdk

--- script/reflect.js
+++ script/reflect.js
@@ -2,8 +2,7 @@
 
 var fs = require('fs');
 var path = require('path');
-var auth = require('../plugins/api-key');
-var request = require('../request')(auth(process.env.FLICKR_API_KEY));
+var flickr = require('..')(process.env.FLICKR_API_KEY);
 
 /**
  * Get the destination filename for `method`
@@ -32,18 +31,18 @@ function stringify(obj) {
  */
 
 function info(method) {
-	return request('GET', 'flickr.reflection.getMethodInfo')
-		.query('method_name=' + method._content)
-		.then(function (res) {
-			fs.writeFileSync(filename(method._content), stringify(res.body));
-		});
+	return flickr.reflection.getMethodInfo({
+		method_name: method._content
+	}).then(function (res) {
+		fs.writeFileSync(filename(method._content), stringify(res.body));
+	});
 }
 
 /**
  * Get info for every method and write them all to disk
  */
 
-request('GET', 'flickr.reflection.getMethods').then(function (res) {
+flickr.reflection.getMethods().then(function (res) {
 	return res.body.methods.method;
 }).then(function (methods) {
 	return Promise.all(methods.map(info));
