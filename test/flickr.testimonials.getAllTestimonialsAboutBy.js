fb6df1df92c7f2a60438408db112694c5c3f4a8a
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2017-08-01 10:18:16 -0700

    Reflect + build

---
+++ test/flickr.testimonials.getAllTestimonialsAboutBy.js
@@ -0,0 +1,29 @@
+var flickr = require('..')(function auth() { /* noop */ });
+var assert = require('assert');
+
+describe('flickr.testimonials.getAllTestimonialsAboutBy', function () {
+
+	it('requires "user_id"', function () {
+
+		assert.throws(function () {
+			flickr.testimonials.getAllTestimonialsAboutBy({});
+		}, function (err) {
+			return err.message === 'Missing required argument "user_id"';
+		});
+
+	});
+
+	it('returns a Request instance', function () {
+		var req = flickr.testimonials.getAllTestimonialsAboutBy({
+			user_id: '_'
+		});
+
+		assert.equal(req.method, 'GET');
+		assert.equal(req.url, 'https://api.flickr.com/services/rest');
+		assert.equal(req.qs.format, 'json');
+		assert.equal(req.qs.nojsoncallback, '1');
+		assert.equal(req.qs.method, 'flickr.testimonials.getAllTestimonialsAboutBy');
+		assert.equal(req.qs.user_id, '_');
+	});
+
+});
